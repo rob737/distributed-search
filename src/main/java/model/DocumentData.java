e0cea04f696b2b9fa56bdd9f7c1eba0ce16e3873
rob737 <robinsrivastava737@gmail.com>
2021-09-12 20:27:03 +0530

    11-09-21 : setup

--- src/main/java/model/DocumentData.java
+++ src/main/java/model/DocumentData.java
@@ -1,4 +1,17 @@
 package model;
 
-public class DocumentData {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DocumentData implements Serializable {
+    private Map<String, Double> termToFrequency = new HashMap<>();
+
+    public void putTermFrequency(String term, double frequency) {
+        termToFrequency.put(term, frequency);
+    }
+
+    public double getFrequency(String term) {
+        return termToFrequency.get(term);
+    }
 }
